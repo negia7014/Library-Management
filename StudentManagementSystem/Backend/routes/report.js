b2020e513e091a59ca23aca87166a901b09921fc
ABHISHEK NEGI <111852535+negia7014@users.noreply.github.com>
2025-10-17 13:44:50 +0530

    Delete StudentManagementSystem/Backend directory

--- StudentManagementSystem/Backend/routes/report.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getReports } = require('../controllers/reportController');
-const { protect, adminOnly } = require('../middleware/auth');
-
-router.get('/', protect, adminOnly, getReports);
-module.exports = router;
--- StudentManagementSystem/Backend/utils/fineCalculator.js
+++
@@ -1,6 +0,0 @@
-const msInDay = 24*60*60*1000;
-export default function calculateFine(returnedDate, expectedDate, perDay){
-if(!returnedDate) return 0;
-const diff = Math.ceil((new Date(returnedDate) - new Date(expectedDate)) / msInDay);
-return diff > 0 ? diff * (perDay||10) : 0;
-};
